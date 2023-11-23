e3163a5079934d9b5fb9b543f107017911975e90
haochenz96 <haochenz1996@gmail.com>
2023-11-23 09:10:19 -0800

    Added support for sub-leaf SNVs

--- condor_downstream/src/ete_tree_style.py
+++ condor_downstream/src/ete_tree_style.py
@@ -28,25 +28,26 @@ def ete_layout(node):
         tf_root.margin_bottom = 10
         ete3.faces.add_face_to_node(tf_root, node, column=0, position='branch-right')
         # return
-    if node.is_leaf():
+    # if node.is_leaf():
+    if 'clone_size_for_plotting' in node.features:
 
         # nstyle['size'] = node.leaf_size / 10
-        # nstyle['fgcolor'] = node.leaf_color
+        # nstyle['fgcolor'] = node.clone_color
         nstyle['size'] = 0
-
-        # a. rectangle face
-        leaf_face = ete3.AttrFace("name", ftype='Arial', fsize=10, fgcolor=node.leaf_color, text_prefix='clone-', text_suffix='')
-        leaf_face.background.color = "White"
-        # leaf_face.border.type = 0
-        # leaf_face.border.width = 0
-        # leaf_face.margin_bottom = 0
-        # leaf_face.margin_top = 0
-        ete3.faces.add_face_to_node(leaf_face, node, column=1, position="branch-right") 
+        if not node.name.startswith("SUB"):
+            # a. rectangle face
+            clone_face = ete3.AttrFace("name", ftype='Arial', fsize=10, fgcolor=node.clone_color, text_prefix='clone-', text_suffix='')
+            clone_face.background.color = "White"
+            # clone_face.border.type = 0
+            # clone_face.border.width = 0
+            # clone_face.margin_bottom = 0
+            # clone_face.margin_top = 0
+            ete3.faces.add_face_to_node(clone_face, node, column=1, position="branch-right") 
 
         # b. circle face
-        leaf_face = ete3.CircleFace(
-            radius = node.leaf_size_for_plotting / 20, 
-            color = node.leaf_color,
+        clone_face = ete3.CircleFace(
+            radius = node.clone_size_for_plotting / 20, 
+            color = node.clone_color,
             style = 'circle',
             # label = {
             #     'text': f"clone-{node.name}",
@@ -55,7 +56,7 @@ def ete_layout(node):
             #     'color': 'Black',
             # }
         )
-        ete3.faces.add_face_to_node(leaf_face, node, column=0, position="branch-right") 
+        ete3.faces.add_face_to_node(clone_face, node, column=0, position="branch-right") 
 
 def add_edge_mutation_events(node):
     # add mutation edge events
@@ -68,6 +69,7 @@ def add_edge_mutation_events(node):
                 
             # add face to branch top
             event_face = ete3.TextFace(f"{event_name_annotated} {event_type}", fsize=8, ftype='Arial', fgcolor=event_color)
+            # event_face.rotation = -30
             ete3.faces.add_face_to_node(event_face, node, column=2, position="branch-top")
         for somatic_snv in node.somatic_snv_events:
             event_name_annotated, event_type, event_color = node.somatic_snv_events[somatic_snv]
@@ -77,20 +79,25 @@ def add_edge_mutation_events(node):
 
             # add face to branch bottom
             event_face = ete3.TextFace(f"{event_name_annotated} {event_type}", fsize=8, ftype='Arial', fgcolor=event_color)
+            # event_face.rotation = -30
             ete3.faces.add_face_to_node(event_face, node, column=2, position="branch-bottom")
 
 
 
 # function to add more features for plotting
 def beautify_tree(
-        ete_tree,
-        cn_clone_size_scale_factor = 800,
-        ):
+    ete_tree,
+    cn_clone_size_scale_factor = 800,
+    ):
     # get all leaves' clone sizes
-    cn_clone_sizes = {int(leaf.name):leaf.leaf_size for leaf in ete_tree.iter_leaves()}
+    cn_clone_sizes = {}
+    for clone in ete_tree.get_descendants():
+        if "clone_size" in clone.features:
+            cn_clone_sizes[clone.name] = clone.clone_size 
     cn_clone_sizes_for_plotting = {k: v / max(cn_clone_sizes.values()) * cn_clone_size_scale_factor for k, v in cn_clone_sizes.items()}
 
-    for leaf in ete_tree.iter_leaves():
-        leaf.add_features(
-            leaf_size_for_plotting = cn_clone_sizes_for_plotting[int(leaf.name)],
-        )
+    for clone in ete_tree.get_descendants():
+        if "clone_size" in clone.features:
+            clone.add_features(
+                clone_size_for_plotting = cn_clone_sizes_for_plotting[clone.name],
+            )
